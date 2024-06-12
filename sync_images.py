5f123b7a3c239062e808c083dddbeef6ffa7096a
llody <745719408@qq.com>
2024-06-12 11:23:23 +0800

    拉取其他组织镜像

--- sync_images.py
+++ sync_images.py
@@ -2,7 +2,7 @@
 Author: llody 745719408@qq.com
 Date: 2024-06-11 18:07:57
 LastEditors: llody 745719408@qq.com
-LastEditTime: 2024-06-12 10:57:56
+LastEditTime: 2024-06-12 11:21:12
 FilePath: \sync-images\.github\workflows\app\sync_images.py
 Description: 批量镜像同步脚本
 '''
@@ -20,16 +20,32 @@ with open("images-to-sync.yml", "r") as stream:
 # 定义目标仓库
 aliyun_registry = "registry.cn-hangzhou.aliyuncs.com"
 huawei_registry = "swr.cn-southwest-2.myhuaweicloud.com"
+namespace = "llody"
+
 # 迁移镜像
 for image_name, tags in images.items():
+    # 提取组织名称和镜像名称
+    if '/' in image_name:
+        org_name, repo_name = image_name.split('/')
+    else:
+        org_name = namespace
+        repo_name = image_name
+    
     for tag_info in tags:
         tag = tag_info["tag"]
         source_image = f"docker://docker.io/{image_name}:{tag}"
-        aliyun_target_image = f"docker://{aliyun_registry}/{image_name}:{tag}"
-        huawei_target_image = f"docker://{huawei_registry}/{image_name}:{tag}"
+        
+        if org_name == namespace:
+            aliyun_target_image = f"docker://{aliyun_registry}/{repo_name}:{tag}"
+            huawei_target_image = f"docker://{huawei_registry}/{repo_name}:{tag}"
+        else:
+            # aliyun_target_image = f"docker://{aliyun_registry}/{namespace}/{org_name}-{repo_name}:{tag}"
+            # huawei_target_image = f"docker://{huawei_registry}/{namespace}/{org_name}-{repo_name}:{tag}"
+            aliyun_target_image = f"docker://{aliyun_registry}/{namespace}/{repo_name}:{tag}"
+            huawei_target_image = f"docker://{huawei_registry}/{namespace}/{repo_name}:{tag}"
         
         try:
-            print(f"Syncing {source_image} to {aliyun_target_image} ")
+            print(f"Syncing {source_image} to {aliyun_target_image}")
             subprocess.run(["skopeo", "copy", "--all", source_image, aliyun_target_image], check=True)
         except subprocess.CalledProcessError as e:
             print(f"Error syncing {source_image} to {aliyun_target_image}: {e}")
