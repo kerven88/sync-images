29c2b87da37def18901ff2e781b6d4407ae64090
llody <745719408@qq.com>
2024-06-12 10:58:19 +0800

    提交镜像迁移

--- sync_images.py
+++ sync_images.py
@@ -2,7 +2,7 @@
 Author: llody 745719408@qq.com
 Date: 2024-06-11 18:07:57
 LastEditors: llody 745719408@qq.com
-LastEditTime: 2024-06-11 18:46:42
+LastEditTime: 2024-06-12 10:57:56
 FilePath: \sync-images\.github\workflows\app\sync_images.py
 Description: 批量镜像同步脚本
 '''
@@ -20,25 +20,23 @@ with open("images-to-sync.yml", "r") as stream:
 # 定义目标仓库
 aliyun_registry = "registry.cn-hangzhou.aliyuncs.com"
 huawei_registry = "swr.cn-southwest-2.myhuaweicloud.com"
-
 # 迁移镜像
 for image_name, tags in images.items():
     for tag_info in tags:
         tag = tag_info["tag"]
-        arch = tag_info.get("arch", "amd64")  # 默认架构为amd64
         source_image = f"docker://docker.io/{image_name}:{tag}"
         aliyun_target_image = f"docker://{aliyun_registry}/{image_name}:{tag}"
         huawei_target_image = f"docker://{huawei_registry}/{image_name}:{tag}"
         
         try:
-            print(f"Syncing {source_image} to {aliyun_target_image} for architecture {arch}")
+            print(f"Syncing {source_image} to {aliyun_target_image} ")
             subprocess.run(["skopeo", "copy", "--all", source_image, aliyun_target_image], check=True)
         except subprocess.CalledProcessError as e:
             print(f"Error syncing {source_image} to {aliyun_target_image}: {e}")
             continue
         
         try:
-            print(f"Syncing {source_image} to {huawei_target_image} for architecture {arch}")
+            print(f"Syncing {source_image} to {huawei_target_image}")
             subprocess.run(["skopeo", "copy", "--all", source_image, huawei_target_image], check=True)
         except subprocess.CalledProcessError as e:
             print(f"Error syncing {source_image} to {huawei_target_image}: {e}")
